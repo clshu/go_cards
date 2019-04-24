5f4e7c069e00353afd27c4fe52bdfb6783fcba8f
clshu <ccss0630@yahoo.com>
2019-04-24 13:39:15 -0700

    Add more tests

--- deck_test.go
+++ deck_test.go
@@ -1,10 +1,14 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 const CardNumber = 16
 const FirstElement = "Ace of Spades"
 const LastElement = "Four of Clubs"
+const CardFile = "go_cards"
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -20,4 +24,18 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != LastElement {
 		t.Errorf("Expected %s but get %s", LastElement, d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove(CardFile)
+
+	deck := newDeck()
+	deck.saveToFile(CardFile)
+
+	loadedDeck := newDeckFromFile(CardFile)
+	if len(deck) != len(loadedDeck) {
+		t.Errorf("Expected deck size %d but get %d", len(deck), len(loadedDeck))
+	}
+
+	os.Remove(CardFile)
 }
